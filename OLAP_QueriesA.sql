5cf48b7c6db1cd8fac3205bfec944a66bdd38ab1
raveena-g <71710220+raveena-g@users.noreply.github.com>
2023-04-11 22:29:40 -0400

    Update OLAP_QueriesA.sql

--- OLAP_QueriesA.sql
+++ OLAP_QueriesA.sql
@@ -1,24 +1,34 @@
   /*Roll up*/
   /*Finding the Years of Potential Life Lost (YPLL) at the year level*/
-  SELECT c.CTYYEAR, SUM(w.YPLL)
-  FROM 
-  wellness_fact as w
+  SELECT c.CTYYEAR, ROUND(CAST(SUM(w.YPLL) AS NUMERIC), 2) AS sum_YPLL
+  FROM
+  wellness_fact w
   LEFT JOIN
-  county_demographics as c
-  ON 
+  county_demographics c
+  ON
   w.county_key = c.county_id
+  WHERE
+  c.CTYYEAR IS NOT NULL
   GROUP BY ROLLUP (c.CTYYEAR)
+  HAVING c.CTYYEAR IS NOT NULL
 
   /*Drill down*/
   /*Finding the YPLL at the year and state level*/
-  SELECT c.CTYYEAR, c.STFIPS, SUM(w.YPLL)
+  SELECT c.CTYYEAR, c.STFIPS, ROUND(CAST(SUM(w.YPLL) AS NUMERIC), 2) AS sum_YPLL
   FROM 
   wellness_fact as w
   LEFT JOIN
   county_demographics as c
   ON 
   w.county_key = c.county_id
+  WHERE
+  c.CTYYEAR IS NOT NULL 
+  AND
+  c.STFIPS IS NOT NULL
   GROUP BY ROLLUP (c.CTYYEAR, c.STFIPS)
+  HAVING c.CTYYEAR IS NOT NULL 
+  AND 
+  c.STFIPS IS NOT NULL 
 
   /*Slice*/
   /*Selecting all measures where the year is 2018*/
@@ -48,7 +58,7 @@
 
   /*Combining OLAP operations*/
   /*Comparing the YPLL in 2018 and 2017*/
-  SELECT c.CTYYEAR, SUM(w.YPLL)
+  SELECT c.CTYYEAR, ROUND(CAST(SUM(w.YPLL) AS NUMERIC), 2) AS sum_YPLL
   FROM 
   wellness_fact as w
   LEFT JOIN
@@ -59,17 +69,28 @@
   GROUP BY ROLLUP (c.CTYYEAR)
 
   /*Determining the average uninsured rate compared to the total female population per year*/
-  SELECT c.CTYYEAR, AVG(w.uninsured), c.TOT_FEMALE
+  SELECT c.CTYYEAR, ROUND(CAST(AVG(w.uninsured) AS NUMERIC), 2) as avg_uninsured, c.TOT_FEMALE
   FROM 
   wellness_fact as w
   LEFT JOIN
   county_demographics as c
   ON 
   w.county_key = c.county_id
+  WHERE
+  c.CTYYEAR IS NOT NULL 
+  AND
+  c.TOT_FEMALE IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
   GROUP BY ROLLUP (c.CTYYEAR, c.TOT_FEMALE, c.FIPS)
+  HAVING c.CTYYEAR IS NOT NULL
+  AND
+  c.TOT_FEMALE IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
 
   /*Determining the average fair or poor health days reported and mean PM2.5 compared to the county population per county per year*/
-  SELECT c.CTYYEAR, c.FIPS, AVG(w.FAIR_POOR_HEALTH) as average_fair_poor_health, c.TOT_POP, AVG(a.mean_PM2_5_Quarterly_Means_of_Daily_Means_mcg_m3_LC) as average_PM25
+  SELECT c.CTYYEAR, c.FIPS, ROUND(CAST(AVG(w.FAIR_POOR_HEALTH) AS NUMERIC), 2) as avg_fair_poor_health, c.TOT_POP, ROUND(CAST(AVG(a.mean_PM2_5_Quarterly_Means_of_Daily_Means_mcg_m3_LC) AS NUMERIC), 2) as avg_PM25
   FROM 
   wellness_fact as w
   LEFT JOIN
@@ -80,10 +101,21 @@
   air_quality_data as a
   ON
   w.air_key = a.air_quality_id
+  WHERE
+  c.CTYYEAR IS NOT NULL 
+  AND
+  c.TOT_POP IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
   GROUP BY ROLLUP (c.CTYYEAR, c.TOT_POP, c.FIPS)
+  HAVING c.CTYYEAR IS NOT NULL
+  AND
+  c.TOT_POP IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
 
   /*Determining the average mentally unheatlhy days and average precipitation per county per year*/
-  SELECT c.CTYYEAR, c.FIPS, AVG(w.MENTALLY_UNHEALTHY) as average_mentally_unhealthy_days, c.TOT_POP, AVG(wd.average_precipitation) as average_precipitation
+  SELECT c.CTYYEAR, c.FIPS, ROUND(CAST(AVG(w.MENTALLY_UNHEALTHY) AS NUMERIC), 2) as avg_mentally_unhealthy, c.TOT_POP, ROUND(CAST(AVG(wd.average_precipitation) AS NUMERIC), 2) as avg_precipitation
   FROM 
   wellness_fact as w
   LEFT JOIN
@@ -94,4 +126,15 @@
   weather_data as wd
   ON
   w.weather_key = wd.weather_id
+  WHERE
+  c.CTYYEAR IS NOT NULL 
+  AND
+  c.TOT_POP IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
   GROUP BY ROLLUP (c.CTYYEAR, c.TOT_POP, c.FIPS)
+  HAVING c.CTYYEAR IS NOT NULL
+  AND
+  c.TOT_POP IS NOT NULL
+  AND
+  c.FIPS IS NOT NULL
